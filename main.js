0e0b00634f6cf45869b9e75874f25c1d81ce8997
Xpanetel <ibrohim@MAC.local>
2024-01-18 23:38:49 +0500

    home

---
+++ main.js
@@ -0,0 +1,86 @@
+//////////////// Object-1 /////////////////
+
+// const me = {
+//   firstName: "Sultonqul",
+//   lastName: "Nortoyloqov",
+//   age: 23,
+//   languages: ["js", "python", "c++", "nodejs"],
+//   friends: ["Jamshid", "Abbos", "Jalol", "Mar'uf"],
+// };
+// console.log(`firstName: ${me.firstName}`);
+// console.log(`lastName: ${me.lastName}`);
+// console.log(`age: ${me.age}`);
+// console.log(`languages: ${me.languages.join(",")}`);
+// console.log(`friends: ${me.friends.join(",")}`);
+
+//////////////// Object-2 /////////////////
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+// for (let i = a; i <= e; i++) {
+//   let k = a ** a;
+//   let l = b ** b;
+//   let m = c ** c;
+//   let n = d ** d;
+//   let o = e ** e;
+//   console.log(k, l, m, n, o);
+// }
+
+//////////////// Object-4 /////////////////
+
+// let = person = {
+//   firstName: "Sultonqul, Safarmurod , O’rol , Jahongir",
+// };
+// person.firstName = "Sultonqul 6, Safarmurod 9, O'rol 5";
+// console.log(person);
+
+//////////////// Object-5 /////////////////
+
+// let fruits = {
+//   "Ja'mi":
+//     ("Apelsin", 10000) +
+//     ("Olma", 12000) +
+//     ("Mandarin", 8000) +
+//     ("Banan", 20000),
+// };
+// console.log(fruits);
+
+//////////////// Object-7 /////////////////
+
+// let a = "Men Sultonqul Programmerning guruhlarida o’qiyman.";
+// minword = a.slice(0, 4);
+// maxword = a.slice(14, 28);
+// console.log("MinWord: ", minword);
+// console.log("MaxWord: ", maxword);
+
+//////////////// Object-9 /////////////////
+
+// const people = [
+//   { name: "Sultonqul", age: 23 },
+//   { name: "Erkin", age: 20 },
+//   { name: "Temur", age: 21 },
+// ];
+// getNameMaxAge = {
+//   name: "Sultonqul",
+// };
+// console.log(getNameMaxAge);
+
+//////////////// Object-17 /////////////////
+
+// const obj = {
+//   firstName: "it",
+//   Age: 20,
+//   lastName: "mushuk",
+//   age: 10,
+//   Name: "sigir",
+//   aGe: 200,
+//   name: "tovuq",
+//   agE: 2,
+// };
+// console.log(` ${obj.firstName}`, `: ${obj.Age}`);
+// console.log(` ${obj.lastName}`, `: ${obj.age}`);
+// console.log(` ${obj.Name}`, `: ${obj.aGe}`);
+// console.log(` ${obj.name}`, `: ${obj.agE}`);
