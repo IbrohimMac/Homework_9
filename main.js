259d2ff0f3455d7e7de529970232e9cf04931179
Xpanetel <ibrohim@MAC.local>
2024-01-19 13:29:51 +0500

    new

--- main.js
+++ main.js
@@ -84,3 +84,80 @@
 // console.log(` ${obj.lastName}`, `: ${obj.age}`);
 // console.log(` ${obj.Name}`, `: ${obj.aGe}`);
 // console.log(` ${obj.name}`, `: ${obj.agE}`);
+
+//////////////// Object-20 /////////////////
+
+// let n = 3;
+// const obj = { a: 2, b: 3, c: 4, d: 6 };
+// let k = 2 + n;
+// let l = 3 + n;
+// let o = 4 + 3;
+// let i = 6 + 3;
+// console.log(k, l, o, i);
+
+//////////////// Object-21 /////////////////
+
+// const product = {
+//   name: "Iphone 14",
+//   company: {
+//     name: "Apple",
+//     price: "200 mlrd",
+//     founder: {
+//       firstName: "Steve",
+//       lastName: "Jobs",
+//       birthDate: 1950,
+//     },
+//   },
+// };
+// console.log(product.company);
+// console.log(product.company.founder);
+
+//////////////// Object-24 /////////////////
+
+// let procent = 70;
+// let right = 69;
+// let res = right >= procent;
+// console.log(res);
+
+//////////////// Object-25 /////////////////
+
+// const pupils = [
+//   {
+//     name: "Elbek",
+//     protcent: 95,
+//   },
+//   {
+//     name: "Zafar",
+//     protcent: 78,
+//   },
+//   {
+//     name: "Aziz",
+//     protcent: 83,
+//   },
+//   {
+//     name: "Jasur",
+//     protcent: 88,
+//   },
+//   {
+//     name: "Bobur",
+//     protcent: 66,
+//   },
+//   {
+//     name: "Kamron",
+//     protcent: 75,
+//   },
+// ];
+
+// let passed = 0;
+// let failed = 0;
+
+// pupils.forEach((pupil) => {
+//   if (pupil.protcent >= 70) {
+//     passed++;
+//   } else {
+//     failed++;
+//   }
+// });
+
+// console.log(`Imtihondan o'tganlar soni: ${passed}`);
+// console.log(`Imtihondan o'ta olmaganlar soni: ${failed}`);
